68ec4d377b573511d2b037b92c03e92e97bdd869
SlenderData <mayunji2001@outlook.com>
2024-06-03 10:23:37 +0800

    Update files

--- src/entity/Goods.java
+++ src/entity/Goods.java
@@ -2,25 +2,30 @@ package entity;
 
 
 public class Goods {
-	
-	private String goodsname;
-	private double price;
-	public Goods() {
-		super();
-		
-	}
-	public String getGoodsname() {
-		return goodsname;
-	}
-	public void setGoodsname(String goodsname) {
-		this.goodsname = goodsname;
-	}
-	public double getPrice() {
-		return price;
-	}
-	public void setPrice(double price) {
-		this.price = price;
-	}
-	
-	
+
+    private String goodsname;
+    private double price;
+
+    public Goods() {
+        super();
+
+    }
+
+    public String getGoodsname() {
+        return goodsname;
+    }
+
+    public void setGoodsname(String goodsname) {
+        this.goodsname = goodsname;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
 }
